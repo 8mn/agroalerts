43c2afddb86b978033aebbbbe6f906876725f79e
calmighted <manishh@tutanota.com>
2022-07-23 00:12:33 +0530

    init, base layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+// 1. get location
+// 2. get weather
+// 3. get phone number
+// 4. send alerts
+// 5. test alerts
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [latitude, setLatitude] = useState("");
+	const [longitude, setLongitude] = useState("");
+	const [weather, setWeather] = useState("");
+
+	// get location
+	// function getLocation() {
+	//   if (navigator.geolocation) {
+	//     console.log('geolocation is available');
+	//     getGeolocation();
+	//   } else {
+	//     console.log("Geolocation is not supported by this browser.");
+	//   }
+	// }
+
+	//get geolocation
+	function getGeolocation() {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			// console.log(position);
+			setLatitude(position.coords.latitude);
+			setLongitude(position.coords.longitude);
+		});
+	}
+
+	useEffect(() => {
+		getGeolocation();
+	}, []);
+
+	useEffect(() => {
+		if (!latitude || !longitude) {
+			return;
+		}
+		getWeather();
+	}, [latitude, longitude]);
+
+	const getWeather = () => {
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+			)
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+
+	};
+
+	return (
+		<div className="App">
+			agro alerts
+			<p>latitude: {latitude}</p>
+			<p>longitude: {longitude}</p>
+		</div>
+	);
 }
 
 export default App;
